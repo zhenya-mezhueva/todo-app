a4059d7f09a226fc33f215fcc5a43f5144eb9eec
Zhenya <e.mezhueva@gmail.com>
2021-02-22 13:43:51 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+
+        <form>
+          <input
+            type="text"
+            className="new-todo"
+            placeholder="What needs to be done?"
+          />
+        </form>
       </header>
-    </div>
+
+      <section className="main">
+        <input type="checkbox" id="toggle-all" className="toggle-all" />
+        <label htmlFor="toggle-all">Mark all as complete</label>
+
+        <ul className="todo-list">
+          <li>
+            <div className="view">
+              <input type="checkbox" className="toggle" />
+              <label>asdfghj</label>
+              <button type="button" className="destroy" />
+            </div>
+            <input type="text" className="edit" />
+          </li>
+
+          <li className="completed">
+            <div className="view">
+              <input type="checkbox" className="toggle" />
+              <label>qwertyuio</label>
+              <button type="button" className="destroy" />
+            </div>
+            <input type="text" className="edit" />
+          </li>
+
+          <li className="editing">
+            <div className="view">
+              <input type="checkbox" className="toggle" />
+              <label>zxcvbnm</label>
+              <button type="button" className="destroy" />
+            </div>
+            <input type="text" className="edit" />
+          </li>
+
+          <li>
+            <div className="view">
+              <input type="checkbox" className="toggle" />
+              <label>1234567890</label>
+              <button type="button" className="destroy" />
+            </div>
+            <input type="text" className="edit" />
+          </li>
+        </ul>
+      </section>
+
+      <footer className="footer">
+        <span className="todo-count">
+          3 items left
+        </span>
+
+        <ul className="filters">
+          <li>
+            <a href="#/" className="selected">All</a>
+          </li>
+
+          <li>
+            <a href="#/active">Active</a>
+          </li>
+
+          <li>
+            <a href="#/completed">Completed</a>
+          </li>
+        </ul>
+
+        <button type="button" className="clear-completed">
+          Clear completed
+        </button>
+      </footer>
+    </section>
   );
 }
 
